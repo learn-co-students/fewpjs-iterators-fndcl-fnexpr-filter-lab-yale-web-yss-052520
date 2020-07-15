6aa121cca03b4b289f5135ff15675d3a4aefb68c
anmolk18 <anmolpreet.kaur@yale.edu>
2020-07-15 19:59:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(array, string){
+    return array.filter(element => element.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(element => element[0] === string[0])
+}
+
+function matchName(array, string){
+    return array.filter(element => element.name === string)
+}
