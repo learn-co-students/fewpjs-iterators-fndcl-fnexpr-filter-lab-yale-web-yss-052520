df15e68df319379335cc6d6a8efa3111741be97b
zayzay500 <lindsay.pierce@yale.edu>
2020-07-16 18:41:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, s) {
+  let match = new RegExp(s, 'i')
+  return drivers.filter(driver => {
+    return driver.match(match)
+  })
+}
+
+function fuzzyMatch(drivers, s) {
+  let match = new RegExp(`^${s}`, 'i')
+  return drivers.filter(driver => {
+    return driver.match(match)
+  })
+}
+
+function matchName(drivers, s) {
+  return drivers.filter(driver => {
+    return driver.name === s
+  })
+}
