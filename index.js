8a1d612ea70cc305012d5294a3f15d7223e0eaa5
alexisteh <wenhui.teh23@gmail.com>
2020-07-17 13:29:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+function findMatching(drivers, string){ 
+    return drivers.filter(driver => {
+        if (driver.toLowerCase() == string.toLowerCase()){
+            return true 
+        } 
+    })
+} 
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter( driver => {
+        if (driver.substring(0, string.length) == string ){
+            return true 
+        }
+    }) 
+} 
+
+function matchName(drivers, string){
+    return drivers.filter(driver => {
+        if(driver.name == string){
+            return true 
+        }
+    })
+}
