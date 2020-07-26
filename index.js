9fb96b8ebb9c52dfd19ea1d59986519b251765e5
tbartelsmeyer <47281160+tbartelsmeyer@users.noreply.github.com>
2020-07-26 17:13:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+// const findMatching = (arr, str) => arr.filter(el => el.toLowerCase() === str.toLowerCase())
+
+function findMatching (arr, str) {
+  return arr.filter(el => el.toLowerCase() === str.toLowerCase())
+}
+
+function fuzzyMatch (arr, str) {
+  let regEx = new RegExp(`^${str.toLowerCase()}`,'g')
+  console.log(regEx)
+  let out = arr.filter( el => regEx.test( el.toLowerCase() ) )
+  console.log(arr.forEach( el => console.log(regEx.test( el.toLowerCase() ) ) ))
+  console.log(`direct test:${regEx.test( " Sally".toLowerCase() )}`)
+  console.log(`out:${out}`)
+  console.log(`arr:${arr}`)
+  return out
+}
+
+function matchName (arr, str) {
+  return arr.filter(el => el.name.toLowerCase() === str.toLowerCase())
+}
